bf976a2709f8be7e6fdbd7e4b5260f5fdc5074f3
Jaswanth374 <jaswanthmarpuri@gmail.com>
2026-02-07 12:33:13 +0530

    roboshop common commit

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -5,15 +5,15 @@ source ./common.sh
 check_root
 
 cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
-VALIDATE $? "Added RabbitMQ repo"
+validate $? "Added RabbitMQ repo"
 
 dnf install rabbitmq-server -y &>>$LOGS_FILE
-VALIDATE $? "Installing RabbitMQ server"
+validate $? "Installing RabbitMQ server"
 
 systemctl enable rabbitmq-server &>>$LOGS_FILE
 systemctl start rabbitmq-server
-VALIDATE $? "Enabled and started rabbitmq"
+validate $? "Enabled and started rabbitmq"
 
 rabbitmqctl add_user roboshop roboshop123 &>>$LOGS_FILE
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOGS_FILE
-VALIDATE $? "created user and gien permissions"
+validate $? "created user and gien permissions"
