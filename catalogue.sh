e6ec439aeff37ee9c18618affd1a50570323a240
Jaswanth374 <jaswanthmarpuri@gmail.com>
2026-02-07 11:46:35 +0530

    roboshop common commit

---
+++ catalogue.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source ./common.sh
+appname=catalogue
+
+check_root
+create_systemuser
+app_code_install
+nodejs
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGS_FILE
+validate $? "Copying the mongo.repo"
+dnf install mongodb-mongosh -y &>>$LOGS_FILE
+validate $? "Install client Mongod in catalogue"
+
+INDEX=$(mongosh --host $MONGODB_HOST --quiet  --eval 'db.getMongo().getDBNames().indexOf("catalogue")') &>>$LOGS_FILE
+
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOGS_FILE
+    validate $? "Loading products"
+else
+    echo -e "Products already loaded ... $Y SKIPPING $N"
+fi
+
+app_restart
--- common.sh
+++ common.sh
@@ -9,6 +9,7 @@ Y="\e[33m"
 N="\e[0m"
 SCRIPT_DIR=$PWD
 mkdir -p $LOGS_FOLDER
+MONGODB_HOST="mongodb.jaswanthdevops.online"
 
 check_root() {
 if [ $USERID -ne 0 ]; then
@@ -17,7 +18,7 @@ if [ $USERID -ne 0 ]; then
 fi
 }
 
-VALIDATE(){
+validate(){
     if [ $1 -ne 0 ]; then
         echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $2 ... $R FAILURE $N" | tee -a $LOGS_FILE
         exit 1
@@ -26,3 +27,58 @@ VALIDATE(){
     fi
 }
 
+create_systemuser(){
+
+    id roboshop &>>$LOGS_FILE
+    if [ $? -ne 0 ]; then
+        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
+        validate $? "Creating system user"
+    else
+        echo -e "Roboshop user already exist ... $Y SKIPPING $N"
+    fi
+
+}
+
+nodejs(){
+
+    dnf module disable nodejs -y &>>$LOGS_FILE
+    validate $? "Disabling NodeJS Default version"
+
+    dnf module enable nodejs:20 -y &>>$LOGS_FILE
+    validate $? "Enabling NodeJS 20"
+
+    dnf install nodejs -y &>>$LOGS_FILE
+    validate $? "Install NodeJS"
+
+    npm install  &>>$LOGS_FILE
+    validate $? "Installing dependencies"
+
+}
+
+app_code_install(){
+
+    mkdir -p /app 
+    validate $? "Creating app directory"
+    curl -o /tmp/$appname.zip https://roboshop-artifacts.s3.amazonaws.com/$appname-v3.zip  &>>$LOGS_FILE
+    validate $? "Downloading $appname code"
+
+    cd /app &>>$LOGS_FILE
+    validate $? "Moving to app directory"
+
+    rm -rf /app/*
+    validate $? "Removing existing code"
+
+    unzip /tmp/$appname.zip &>>$LOGS_FILE
+    validate $? "Uzip $appname code"
+}
+
+systemed(){
+
+    cp $SCRIPT_DIR/$appname.service /etc/systemd/system/$appname.service
+    validate $? "Created systemctl service"
+
+    systemctl daemon-reload
+    systemctl enable $appname  &>>$LOGS_FILE
+    systemctl start $appname
+    validate $? "Starting and enabling $appname"
+}
