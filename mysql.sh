4cef5ebd7ccf3b98af8f033a03ee9af69db8edeb
Jaswanth374 <jaswanthmarpuri@gmail.com>
2026-02-07 12:17:02 +0530

    roboshop common commit

--- mysql.sh
+++ mysql.sh
@@ -5,13 +5,13 @@ source ./common.sh
 check_root
 
 dnf install mysql-server -y &>>$LOGS_FILE
-VALIDATE $? "Installation of mysql"
+validate $? "Installation of mysql"
 
 systemctl enable mysqld &>>$LOGS_FILE
 systemctl start mysqld &>>$LOGS_FILE
-VALIDATE $? "started mysql services"
+validate $? "started mysql services"
 
 #set a password through read command
 
 mysql_secure_installation --set-root-pass RoboShop@1
-VALIDATE $? "set mysql root password"
+validate $? "set mysql root password"
